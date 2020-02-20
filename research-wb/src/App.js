e1c7e82c2900ccbe28b949d56cd7e8a35a5a17d9
mcherna2 <46982245+mcherna2@users.noreply.github.com>
2020-02-20 12:18:09 -0800

    initial website frame commit

---
+++ research-wb/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <iframe src="https://search-evokerb-c4pasf6ebotkehmtqwin3ovhgu.us-east-2.es.amazonaws.com/_plugin/kibana/app/kibana#/dashboard/525e83d0-4ee3-11ea-b552-5171c403214e?embed=true&_g=()" 
+        height="800" width="1600"></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
